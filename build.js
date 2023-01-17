be460d98b5b31a187059483d0f18a5496fa1fb45
m4rcus <marcuspatock@gmail.com>
2023-01-18 00:18:25 +0100

    Update build.js

--- build.js
+++ build.js
@@ -2,41 +2,20 @@ const StyleDictionaryPackage = require('style-dictionary');
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
-StyleDictionaryPackage.registerFormat({
-    name: 'css/variables',
-    formatter: function (dictionary, config) {
-      return `${this.selector} {
-        ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
-      }`
-    }
-  });  
-
-StyleDictionaryPackage.registerTransform({
-    name: 'sizes/px',
-    type: 'value',
-    matcher: function(prop) {
-        // You can be more specific here if you only want 'em' units for font sizes    
-        return ["fontSize", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
-    },
-    transformer: function(prop) {
-        // You can also modify the value here if you want to convert pixels to ems
-        return parseFloat(prop.original.value) + 'px';
-    }
-    });
-
-function getStyleDictionaryConfig(theme) {
+function getStyleDictionaryConfig(brand, platform) {
   return {
     "source": [
-      `tokens/${theme}.json`,
+      `tokens/${brand}.json`
     ],
     "platforms": {
-      "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `css/`,
+      "css": {
+        "transformGroup": "css",
+        "buildPath": "css/",
         "files": [{
-            "destination": `${theme}.css`,
-            "format": "css/variables"          }]
-      }
+          "destination": `${brand}.css`,
+          "format": "css/variables"
+        }]
+      },
     }
   };
 }
@@ -45,16 +24,19 @@ console.log('Build started...');
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-['input', 'bf', 'mw'].map(function (theme) {
+['input', 'bf', 'mw'].map(function (brand) {
+  ['css'].map(function (platform) {
 
     console.log('\n==============================================');
-    console.log(`\nProcessing: [${theme}]`);
+    console.log(`\nProcessing: [${platform}] [${brand}]`);
 
-    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
+    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(brand, platform));
 
-    StyleDictionary.buildPlatform('web');
+    StyleDictionary.buildPlatform(platform);
 
     console.log('\nEnd processing');
+
+  })
 })
 
 console.log('\n==============================================');
